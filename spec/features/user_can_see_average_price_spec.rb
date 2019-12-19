e42a06ebd3dfb480868e81592f560b01f33e2292
Linda Le <yueling1222@gmail.com>
2019-12-19 09:52:59 -0700

    Add test for story 2

--- spec/features/user_can_see_average_price_spec.rb
+++ spec/features/user_can_see_average_price_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
   scenario 'see name and price of all snacks in vending machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
-    soda = dons.snacks.create(name: "Orange Soda", price: 1.50)
-    chips = dons.snacks.create(name: "Cheetos", price: 0.75)
+    soda = dons.snacks.create(name: "Dr.Pepper", price: 1.25)
+    snickers = dons.snacks.create(name: "Snickers", price: 1.75)
 
     visit machine_path(dons)
-    expect(page).to have_content("#{soda.name}: $1.50")
-    expect(page).to have_content("#{chips.name}: $0.75")
+    expect(page).to have_content("Average Price: $1.50")
   end
 end
